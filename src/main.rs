42a7cb3ddecffbe3ab9f051721c24ea7e4231bb2
Roman Roibu <roman@roibu.xyz>
2024-04-30 21:40:40 +0200

    Clean-up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use futures::StreamExt;
 
+const UNI_V3_DAI_USDC_POOL: &str = "5777d92f208679db4b9778590fa3cab3ac9e2168";
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
 	dotenv::dotenv().ok();
@@ -8,20 +10,34 @@ async fn main() -> Result<(), anyhow::Error> {
 
 	let web3 = web3::Web3::new(web3::transports::ws::WebSocket::new(ws_url).await?);
 
-	let contract_address = web3::types::H160::from_slice(
-		&hex::decode("5777d92f208679db4b9778590fa3cab3ac9e2168").unwrap()[..],
-	);
+	let contract_address = hex::decode(UNI_V3_DAI_USDC_POOL).unwrap();
+	let contract_address = web3::types::H160::from_slice(&contract_address[..]);
+
 	let contract = web3::contract::Contract::from_json(
 		web3.eth(),
 		contract_address,
 		include_bytes!("contracts/uniswap_pool_abi.json"),
 	)?;
-	let swap_event = contract.abi().events_by_name("Swap")?.first().unwrap();
-	let swap_event_signature = swap_event.signature();
+
+	let swap_event_abi = contract.abi().events_by_name("Swap")?.first().unwrap();
+
+	let swap_event_signature = swap_event_abi.signature();
 
 	let mut block_stream = web3.eth_subscribe().subscribe_new_heads().await?;
 
-	while let Some(Ok(block)) = block_stream.next().await {
+	loop {
+		let block = match block_stream.next().await {
+			Some(Ok(block)) => block,
+			_ => continue,
+		};
+
+		let block_number = match block.number {
+			Some(number) => number,
+			_ => continue,
+		};
+
+		println!("BLOCK {:?}", block_number);
+
 		let swap_logs_in_block = web3
 			.eth()
 			.logs(
@@ -34,11 +50,11 @@ async fn main() -> Result<(), anyhow::Error> {
 			.await?;
 
 		for log in swap_logs_in_block {
-			let parsed_log = swap_event
-				.parse_log(web3::ethabi::RawLog { topics: log.topics, data: log.data.0 })?;
-			println!("{:?}", parsed_log);
+			let log = web3::ethabi::RawLog { topics: log.topics, data: log.data.0 };
+			let log = swap_event_abi.parse_log(log)?;
+			println!("{:?}", log);
 		}
-	}
 
-	Ok(())
+		println!("---")
+	}
 }
